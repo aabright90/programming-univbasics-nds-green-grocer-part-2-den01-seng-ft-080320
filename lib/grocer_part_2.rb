c27ab16630819a11842f46dfdfa6f54a3ed75bd8
Austen Bright <pureaustenbright@gmail.com>
2020-04-18 19:05:32 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -56,9 +56,9 @@ def checkout(cart, coupons)
   total = 0
   new_cart = consolidate_cart(cart)
   consolidate_cart(cart).each do |item|
+    total += item[:price]
     apply_coupons(new_cart, coupons)
     apply_clearance(new_cart)
-    total += item[:price]
     # binding.pry
   end
   total
