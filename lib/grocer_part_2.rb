5354090a33e8a6e7ca2c0edd549aee2ff75169b0
Austen Bright <pureaustenbright@gmail.com>
2020-04-18 17:40:04 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -53,4 +53,5 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  new_consolidates_cart = consolidate_cart(cart)
 end
